76af1ce34641f4cd69232035896ba66d06d17c83
Werllen Castro <hi@werls.xyz>
2023-09-06 16:05:35 -0300

    add popup component

---
+++ src/components/Popup.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let message = '';
+	function close() {
+		document.getElementById('btnClose').parentElement.remove();
+	}
+</script>
+
+<div class="bg-slate-200 max-w-3xl rounded-md px-5 py-4 m-2 absolute bottom-0 flex justify-between gap-3">
+	<slot />
+	<button id="btnClose" on:click={close} class=""><i class="material-icons hover:text-slate-400">close</i></button>
+</div>
