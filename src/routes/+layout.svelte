d7e92feb510566a49b5e6ae6c8f1a76cece5a7c8
Werllen Castro <hi@werls.xyz>
2023-09-06 15:45:57 -0300

    update author name

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 
 <header class="bg-slate-300 p-3 flex justify-between items-baseline">
   <h1 class="text-xl font-medium">Escala Tipográfica</h1>
-  <p class="text-slate-200"><a class="hover:text-slate-500" href="https://werls.xyz">Werls, 2023</a></p>
+  <p class="text-slate-200"><a class="hover:text-slate-500 text-sm" href="https://werls.xyz">Werllen Castro, 2023</a></p>
 </header>
 
 <slot />
