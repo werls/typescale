6edf3c662452399bc229ab2a6e8351213bbaf369
Werllen Castro <hi@werls.xyz>
2023-09-07 11:10:53 -0300

    add popup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
 <script>
   import "../app.css";
+  import Popup from "../components/Popup.svelte";
 </script>
 
 <header class="bg-slate-300 p-3 flex justify-between items-baseline">
   <h1 class="text-xl font-medium">Escala Tipográfica</h1>
-  <p class="text-slate-200"><a class="hover:text-slate-500 text-sm" href="https://werls.xyz">Werllen Castro, 2023</a></p>
+  <p class="text-slate-100"><a class="hover:text-slate-500 text-sm" href="https://werls.xyz">Werllen Castro, 2023</a></p>
 </header>
 
 <slot />
+
+<!-- <Popup>
+  <div>
+    Este aplicativo ainda está em fase de testes. Encontrou algum bug? 
+    Entre em contato pelo e-mail 
+    <a class="underline" href="mailto:hi@werls.xyz">hi@werls.xyz</a>
+    ou abra uma Issue no <a class="underline" href="https://github.com/werls/typescale">repositório do projeto no GitHub</a>.
+  </div>
+</Popup> -->
+
