fde0e4db8090b19ad2f6efe76b0ac9dc2b11d119
Werllen <hi@werls.xyz>
2023-09-13 21:15:43 -0300

    set transition for text size changes

--- src/components/LineOfType.svelte
+++ src/components/LineOfType.svelte
@@ -1,20 +1,25 @@
 <script>
   import { unit } from '../stores.js';
   import { onMount } from 'svelte';
-	export let item;
+
+  export let item;
+
+  let fontSize;
+
+  $: fontSize = `${item.size}${$unit}`;
 
   onMount(() => {
-    console.log($unit)
-  })
+    console.log($unit);
+  });
 </script>
 
 <div class="p-3 text-slate-400 w-full">
   <div class="grid grid-cols-10 items-baseline gap-2">
     <div class="text-xs col-span-1">{item.type}</div>
-    <div style="font-size:{item.size + $unit}" class="text-scale text-gray-900 line-clamp-1 col-span-8">
+    <div style="font-size:{fontSize}" class="text-scale text-gray-900 line-clamp-1 col-span-8 transition-all duration-500">
       <slot />
     </div>
-    <div class="text-xs col-span-1 justify-self-end">{item.size + ' ' + $unit}</div>
+    <div class="text-xs col-span-1 justify-self-end">{fontSize}</div>
   </div>
   <hr class="border-gray-300" />
 </div>
