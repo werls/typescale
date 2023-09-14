411f04b16feed5870fff989e47175f65d84a9efc
Werllen <hi@werls.xyz>
2023-09-13 21:05:19 -0300

    add documentation

--- src/lib/typeScale.js
+++ src/lib/typeScale.js
@@ -1,4 +1,14 @@
+/**
+ * Represents a type scale object that generates a typographic scale based on a base size and a scale factor.
+ */
 export class TypeScale {
+	/**
+	 * Creates a new TypeScale object.
+	 * @param {number} baseSize_ - The base font size in pixels or points.
+	 * @param {number} scale_ - The scale factor used to generate the font sizes.
+	 * @param {number} levels_ - The number of heading levels to generate (default is 4).
+	 * @param {string} unit_ - The unit of measurement for the base size (default is 'px').
+	 */
 	constructor(baseSize_, scale_, levels_ = 4, unit_ = 'px') {
 		this.baseSize = baseSize_
 		this.scale = scale_
@@ -13,6 +23,14 @@ export class TypeScale {
 		this.createHeadings(this.levels)
 	}
 
+	/**
+	 * Generates a type scale object based on a base size and a scale factor.
+	 * @param {number} baseSize_ - The base font size in pixels or points.
+	 * @param {number} scale_ - The scale factor used to generate the font sizes.
+	 * @param {number} levels_ - The number of heading levels to generate (default is 4).
+	 * @param {string} unit_ - The unit of measurement for the base size (default is 'px').
+	 * @returns {object} - The generated type scale object.
+	 */
 	static generateTypeScale(baseSize_, scale_, levels_ = 4, unit_ = 'px') {
 		const ptToPx = (pt) => { return (pt * 1.3333333333333) }
 		const pxToPt = (px) => { return (px / 1.3333333333333) }
@@ -49,6 +67,10 @@ export class TypeScale {
 		return typeScale
 	}
 
+	/**
+	 * Creates the heading levels for the type scale object.
+	 * @param {number} levels - The number of heading levels to generate.
+	 */
 	createHeadings(levels) {
 		for (let i = 1; i <= levels; i++) {
 			this.typeScale.headings.push({
@@ -65,18 +87,36 @@ export class TypeScale {
 		});
 	}
 
+	/**
+	 * Returns the type scale object.
+	 * @returns {object} - The type scale object.
+	 */
 	getTypeScale() {
 		return this.typeScale
 	}
 
+	/**
+	 * Returns the heading levels of the type scale object.
+	 * @returns {array} - The heading levels of the type scale object.
+	 */
 	getHeadings() {
 		return this.typeScale.headings
 	}
 
+	/**
+	 * Returns the body font size of the type scale object.
+	 * @returns {object} - The body font size of the type scale object.
+	 */
 	getBody() {
 		return this.typeScale.body
 	}
 
+	/**
+	 * Updates the type scale object with new base size, scale factor and number of heading levels.
+	 * @param {number} baseSize_ - The new base font size in pixels or points.
+	 * @param {number} scale_ - The new scale factor used to generate the font sizes.
+	 * @param {number} levels_ - The new number of heading levels to generate.
+	 */
 	updateTypeScale(baseSize_, scale_, levels_) {
 		this.baseSize = baseSize_
 		this.scale = scale_
