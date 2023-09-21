48ce11927f6a07598df5e77d2acaaf409198575f
Werllen Castro <hi@werls.xyz>
2023-09-21 15:52:51 -0300

    update the function to avoid multiple stylesheets

--- src/lib/updateFont.js
+++ src/lib/updateFont.js
@@ -1,7 +1,15 @@
 export function updateFont(font) {
   const head = document.querySelector("head");
-  const fontSheet = document.createElement("link");
-  fontSheet.rel = "stylesheet";
-  fontSheet.href = `https://fonts.googleapis.com/css2?family=${font}:wght@400;500;600;700;800&display=swap`;
-  head.appendChild(fontSheet);
+  let fontSheet = document.getElementById("font-sheet");
+
+  if (!fontSheet) {
+    const fontSheet = document.createElement("link");
+    fontSheet.id = "font-sheet"
+    fontSheet.rel = "stylesheet";
+    
+    fontSheet.href = `https://fonts.googleapis.com/css2?family=${font}:wght@400;500;600;700;800&display=swap`;
+    head.appendChild(fontSheet);
+  } else {
+    fontSheet.href = `https://fonts.googleapis.com/css2?family=${font}:wght@400;500;600;700;800&display=swap`;
+  }
 }
