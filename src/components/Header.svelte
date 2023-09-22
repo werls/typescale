f8e5af1641fa03082e14a57420d4c8bb6fe41e57
Werllen Castro <hi@werls.xyz>
2023-09-22 13:34:00 -0300

    remove author reference on header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,5 +3,5 @@
     <img src="typescale-icon.svg" alt=""/>
     <h1 class="text-lg text-black p-1">Escala Tipográfica</h1>
   </div>
-  <p class="text-slate-100"><a class="hover:text-slate-500 text-sm" href="https://werls.xyz">Werllen Castro, 2023</a></p>
+  <!-- <p class="text-slate-100"><a class="hover:text-slate-500 text-sm" href="https://werls.xyz">Werllen Castro, 2023</a></p> -->
 </header>
